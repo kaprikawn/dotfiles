2c99b7f9ae8f5ea76c427c4b675efbe2c7bc2cfa
kaprikawn <wayne@kaprikawn.co.uk>
2017-05-29 09:38:44 +0100

    Update pathogen.sh

--- pathogen.sh
+++ pathogen.sh
@@ -2,3 +2,6 @@
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
